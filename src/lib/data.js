2cbe54762322f3114f3981d15637147da7f69c32
yoony97 <yoony97@github.com>
2023-12-04 16:32:50 +0900

    add layout and data format

---
+++ src/lib/data.js
@@ -0,0 +1,31 @@
+export const menuItems = [
+    {
+        title: "News",
+        href: "/topic/news",
+    },
+    {
+        title: "Reviews",
+        href: "/topic/reviews",
+    },
+    {
+        title: "Best",
+        href: "/topic/best",
+    },
+    {
+        title: "Devices",
+        href: "/topic/devices",
+    },
+    {
+        title: "About",
+        href: "/about",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    },
+];
+
+export const siteInfo = {
+    title: "AI TECH STUDY",
+    description: "개인 공부용 레포",
+};
