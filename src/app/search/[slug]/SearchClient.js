b53ab34f88235687324b8f732a2320cfd2c61ce0
yoony97 <82javaking@gmail.com>
2024-07-14 16:37:06 +0900

    FIX ERROR

---
+++ src/app/search/[slug]/SearchClient.js
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from 'react';
+import { allPosts } from "@/.contentlayer/generated";
+import styles from "./Search.module.css";
+import { useRouter } from 'next/router';
+import { compareDesc } from "date-fns";
+import PostPreview from "@/components/PostPreview/PostPreview";
+
+function SearchClient() {
+    const [keyword, setKeyword] = useState('');
+    const [posts, setPosts] = useState([]);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.query.keyword) {
+            const decodedKeyword = decodeURIComponent(router.query.keyword);
+            setKeyword(decodedKeyword);
+
+            const filteredPosts = allPosts
+                .filter((post) => {
+                    const titleInLowerCase = post.title.toString().toLowerCase();
+                    return titleInLowerCase.includes(decodedKeyword);
+                })
+                .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+
+            setPosts(filteredPosts);
+        }
+    }, [router.query.keyword]);
+
+    const postPreviews = posts.map((post, idx) => (
+        <PostPreview key={idx} {...post} />
+    ));
+
+    return (
+        <div className={styles.SearchPageContainer}>
+            <h1>Search Results for &quot;{keyword}&quot;</h1>
+            <div className="ListPosts">{postPreviews}</div>
+        </div>
+    );
+}
+
+export default SearchClient;
--- src/app/search/[slug]/page.js
+++ src/app/search/[slug]/page.js
@@ -1,43 +1,18 @@
-"use client";
-
-import { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
 import { allPosts } from "@/.contentlayer/generated";
-import styles from "./Search.module.css";
-import { useRouter } from 'next/router';
-import { compareDesc } from "date-fns";
-import PostPreview from "@/components/PostPreview/PostPreview";
 
-function Search() {
-    const [keyword, setKeyword] = useState('');
-    const [posts, setPosts] = useState([]);
-    const router = useRouter();
+const SearchClient = dynamic(() => import('./SearchClient'), { ssr: false });
 
-    useEffect(() => {
-        if (router.query.keyword) {
-            const decodedKeyword = decodeURIComponent(router.query.keyword);
-            setKeyword(decodedKeyword);
+export const generateStaticParams = async () => {
+  const slugs = allPosts.map(post => ({ slug: post._raw.flattenedPath }));
+  return slugs;
+};
 
-            const filteredPosts = allPosts
-                .filter((post) => {
-                    const titleInLowerCase = post.title.toString().toLowerCase();
-                    return titleInLowerCase.includes(decodedKeyword);
-                })
-                .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+export const generateMetadata = ({ params }) => {
+  const { slug } = params;
+  return { title: `Search results for ${slug}` };
+};
 
-            setPosts(filteredPosts);
-        }
-    }, [router.query.keyword]);
-
-    const postPreviews = posts.map((post, idx) => (
-        <PostPreview key={idx} {...post} />
-    ));
-
-    return (
-        <div className={styles.SearchPageContainer}>
-            <h1>Search Results for &quot;{keyword}&quot;</h1>
-            <div className="ListPosts">{postPreviews}</div>
-        </div>
-    );
+export default function SearchPage({ params }) {
+  return <SearchClient params={params} />;
 }
-
-export default Search;
